63c68d5ce1c59d30885c27dde765c4f9f06d386b
mohsinalimughal <mohsinalimughal112233445566@gmail.com>
2024-10-26 13:24:53 -0700

    pk

--- app.js
+++ app.js
@@ -1,260 +1,32 @@
 const productsarray = [
-    {
-      "name": "Smartphone Protection Gear",
-      "model": "ArmorShield X3 Case",
-      "price": 19.99
-    },
-    {
-      "name": "Battery Life Enhancer",
-      "model": "PowerSaver Pro",
-      "price": 4.99
-    },
-    {
-      "name": "Camera Lens Attachments",
-      "model": "SnapLens Kit",
-      "price": 29.99
-    },
-    {
-      "name": "Mobile Payment Solution",
-      "model": "QuickPay Secure",
-      "price": 0.99
-    },
-    {
-      "name": "Modular Accessories",
-      "model": "ModiX Attach",
-      "price": 24.99
-    },
-    {
-      "name": "Smartphone Sanitizer",
-      "model": "SanitiMate UV",
-      "price": 39.99
-    },
-    {
-      "name": "Interactive Home Screen Widgets",
-      "model": "WidgetPro",
-      "price": 1.99
-    },
-    {
-      "name": "Personalization App",
-      "model": "ThemeLab Customizer",
-      "price": 2.99
-    },
-    {
-      "name": "Phone Mounting System",
-      "model": "MountEase Flex",
-      "price": 15.99
-    },
-    {
-      "name": "Health Monitoring Accessories",
-      "model": "HealthSync Monitor",
-      "price": 49.99
-    },
-    {
-      "name": "Wireless Charger",
-      "model": "ChargeMate X1",
-      "price": 25.99
-    },
-    {
-      "name": "Portable Power Bank",
-      "model": "PowerGo 10K",
-      "price": 34.99
-    },
-    {
-      "name": "Selfie Stick",
-      "model": "SelfieSnap Pro",
-      "price": 12.99
-    },
-    {
-      "name": "Bluetooth Earbuds",
-      "model": "AudioFit",
-      "price": 45.99
-    },
-    {
-      "name": "Gaming Controller",
-      "model": "GameGrip One",
-      "price": 29.99
-    },
-    {
-      "name": "Noise Cancelling Headphones",
-      "model": "SilenceMax",
-      "price": 79.99
-    },
-    {
-      "name": "Anti-glare Screen Protector",
-      "model": "ClearView Anti-Glare",
-      "price": 9.99
-    },
-    {
-      "name": "Phone Ring Holder",
-      "model": "RingEasy 360",
-      "price": 5.99
-    },
-    {
-      "name": "Car Charger",
-      "model": "AutoCharge Turbo",
-      "price": 14.99
-    },
-    {
-      "name": "Virtual Reality Headset",
-      "model": "VRLite",
-      "price": 49.99
-    },
-    {
-      "name": "Magnetic Cable Organizer",
-      "model": "CableKeep",
-      "price": 7.99
-    },
-    {
-      "name": "Portable Speaker",
-      "model": "SoundBlast Mini",
-      "price": 39.99
-    },
-    {
-      "name": "Fast Charging Cable",
-      "model": "SpeedLink Pro",
-      "price": 8.99
-    },
-    {
-      "name": "Waterproof Phone Pouch",
-      "model": "AquaSafe",
-      "price": 10.99
-    },
-    {
-      "name": "Camera Stabilizer",
-      "model": "SmoothCapture",
-      "price": 89.99
-    },
-    {
-      "name": "Bluetooth Keyboard",
-      "model": "TypeOnGo",
-      "price": 25.99
-    },
-    {
-      "name": "PopSocket",
-      "model": "GripPop",
-      "price": 9.99
-    },
-    {
-      "name": "Wireless Earphones Case",
-      "model": "EarGuard",
-      "price": 12.99
-    },
-    {
-      "name": "Phone Tripod",
-      "model": "SteadyPod",
-      "price": 19.99
-    },
-    {
-      "name": "Privacy Screen Protector",
-      "model": "ViewGuard",
-      "price": 11.99
-    },
-    {
-      "name": "Thermal Imaging Camera",
-      "model": "HeatVision",
-      "price": 99.99
-    },
-    {
-      "name": "Extended Battery Case",
-      "model": "PowerCase Max",
-      "price": 39.99
-    },
-    {
-      "name": "Portable Photo Printer",
-      "model": "Printy",
-      "price": 79.99
-    },
-    {
-      "name": "Memory Card Reader",
-      "model": "MemoryConnect",
-      "price": 15.99
-    },
-    {
-      "name": "Stylus Pen",
-      "model": "TouchPen",
-      "price": 5.99
-    },
-    {
-      "name": "Mini Projector",
-      "model": "PocketProject",
-      "price": 59.99
-    },
-    {
-      "name": "Anti-theft Phone Alarm",
-      "model": "SafePhone",
-      "price": 6.99
-    },
-    {
-      "name": "Temperature Sensor",
-      "model": "ThermoSense",
-      "price": 19.99
-    },
-    {
-      "name": "Fingerprint Lock Case",
-      "model": "SecureLock",
-      "price": 45.99
-    },
-    {
-      "name": "360 Camera Attachment",
-      "model": "Panorama360",
-      "price": 89.99
-    },
-    {
-      "name": "Wireless Charging Stand",
-      "model": "StandCharge",
-      "price": 29.99
-    },
-    {
-      "name": "LED Selfie Ring Light",
-      "model": "GlowRing",
-      "price": 14.99
-    },
-    {
-      "name": "Foldable Bluetooth Keyboard",
-      "model": "FoldType",
-      "price": 35.99
-    },
-    {
-      "name": "Solar Power Bank",
-      "model": "SunCharge",
-      "price": 29.99
-    },
-    {
-      "name": "Phone Cleaning Kit",
-      "model": "CleanSwipe",
-      "price": 6.99
-    },
-    {
-      "name": "Portable Fan Attachment",
-      "model": "BreezeClip",
-      "price": 7.99
-    },
-    {
-      "name": "Screen Magnifier",
-      "model": "MaxView",
-      "price": 12.99
-    },
-    {
-      "name": "UV-C Sanitizing Case",
-      "model": "PureCase",
-      "price": 39.99
-    },
-    {
-      "name": "Headphone Splitter",
-      "model": "ShareTunes",
-      "price": 5.99
-    },
-    {
-      "name": "Digital Thermometer",
-      "model": "TempMate",
-      "price": 12.99
-    },
-    {
-      "name": "Infrared Remote Control",
-      "model": "SmartControl",
-      "price": 9.99
-    }
-  ]
+  { name: "iPhone", model: "13 Pro Max", price: 1099, image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-max-hero?wid=940&hei=1112&fmt=png-alpha&.v=1645552346295" },
+  { name: "Samsung Galaxy", model: "S22 Ultra", price: 1199, image: "https://images.samsung.com/is/image/samsung/p6pim/levant/galaxy-s22/gallery/levant-galaxy-s22-ultra-s908-sm-s908elgimea-530157073?$PD_GALLERY_L_JPG$" },
+  { name: "Google Pixel", model: "6 Pro", price: 899, image: "https://lh3.googleusercontent.com/gTT5jHzAihLy_tYGJh5_hXZx-sVvAEtOZKUQ92y2H8DszmpTt2jkDXs_Cx3gvduRA2XNxZChOT8U1LbA7jjDQ30U=w1200" },
+  { name: "OnePlus", model: "9 Pro", price: 799, image: "https://image01.oneplus.net/ebp/202203/25/2053/545e2d967f28d93ccf501b5d89168e10.png" },
+  { name: "Sony Xperia", model: "1 III", price: 1299, image: "https://m.media-amazon.com/images/I/71yk4KnI8XS._AC_SY679_.jpg" },
+  { name: "Motorola Edge", model: "20 Pro", price: 699, image: "https://motorolaus.vtexassets.com/arquivos/ids/157690-800-800?v=637850853625330000&width=800&height=800&aspect=true" },
+  { name: "Nokia", model: "G50", price: 299, image: "https://www.nokia.com/sites/default/files/styles/scale_720w/public/nokia_g50-blue_product_image_1.png" },
+  { name: "Asus ROG", model: "Phone 5", price: 999, image: "https://dlcdnwebimgs.asus.com/gain/fe787d1b-988a-4a7c-8359-f5d429d1bb65/" },
+  { name: "Xiaomi", model: "Mi 11", price: 749, image: "https://i01.appmifile.com/webfile/globalimg/0320/TO-B/cms/mi11_01.jpg" },
+  { name: "Huawei", model: "P50 Pro", price: 1049, image: "https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/pdp/phones/p50-pro/list-image/p50pro_golden_photos.jpg" },
+  { name: "Realme", model: "GT", price: 599, image: "https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/realme-gt/n5-gt-family-black.png" },
+  { name: "Oppo", model: "Find X3 Pro", price: 1099, image: "https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/find-x3-pro/navigation/n2-find-x3-pro-black-v2-720x720.jpg" },
+  { name: "Vivo", model: "X70 Pro+", price: 1099, image: "https://fdn2.gsmarena.com/vv/pics/vivo/vivo-x70-pro-plus-1.jpg" },
+  { name: "LG", model: "Wing", price: 899, image: "https://m.media-amazon.com/images/I/71gbWeq8s0L._AC_SX679_.jpg" },
+  { name: "Honor", model: "50", price: 499, image: "https://fdn2.gsmarena.com/vv/pics/honor/honor-50-1.jpg" },
+  { name: "ZTE", model: "Axon 30 Ultra", price: 749, image: "https://fdn2.gsmarena.com/vv/pics/zte/zte-axon-30-ultra-1.jpg" },
+  { name: "Blu", model: "G90 Pro", price: 249, image: "https://m.media-amazon.com/images/I/61mkFwUuWDL._AC_SY741_.jpg" },
+  { name: "Infinix", model: "Zero X Pro", price: 299, image: "https://fdn2.gsmarena.com/vv/pics/infinix/infinix-zero-x-pro-1.jpg" },
+  { name: "Meizu", model: "18 Pro", price: 799, image: "https://fdn2.gsmarena.com/vv/pics/meizu/meizu-18-pro-2.jpg" },
+  { name: "Sharp", model: "Aquos R6", price: 999, image: "https://fdn2.gsmarena.com/vv/pics/sharp/sharp-aquos-r6.jpg" },
+  { name: "Fairphone", model: "4", price: 579, image: "https://fdn2.gsmarena.com/vv/pics/fairphone/fairphone-4-5g.jpg" },
+  { name: "TCL", model: "10 Pro", price: 449, image: "https://m.media-amazon.com/images/I/71WiZUk07PL._AC_SY679_.jpg" },
+  { name: "Lava", model: "Z6", price: 139, image: "https://m.media-amazon.com/images/I/71npDLOVZNL._AC_SY679_.jpg" },
+  { name: "Vsmart", model: "Live 4", price: 249, image: "https://fdn2.gsmarena.com/vv/pics/vsmart/vsmart-live-4.jpg" },
+  { name: "Tecno", model: "Camon 16", price: 159, image: "https://m.media-amazon.com/images/I/71ti5ybk+LL._AC_SX679_.jpg" }
+];
+
+
   
 
 
@@ -286,9 +58,10 @@ const checkout = ()=>{
 
 
 
-productsarray.map((item,index,Array)=>{
+productsarray.map((item,index)=>{
     productsdiv.innerHTML += `            
            <div class="card">
+           <img src=${item.image} alt="">
             <h2>${item.name}</h2>
             <h2>${item.model}</h2>
             <h3>price : $${item.price}</h3>
--- cart.js
+++ cart.js
@@ -42,37 +42,40 @@ const deleteitem = (index)=>{
       })
 }
 
-const minusitem = (item,index)=>{
-        // productsdiv.innerHTML = `` 
-        item.quantity +=
+const minusitem = (index)=>{
+    productsdiv.innerHTML = `` 
+    cart[index].quantity = cart[index].quantity - 1
+    cart.map((item,index)=>{
         productsdiv.innerHTML += `             
-        <div class="card">
-        <h2>${item.name}</h2>
-        <h2>${item.model}</h2>
-        <h3>price : ${item.price}</h3>
-        <div class="quantity-box">
-            <h2 onclick="plusitem(${index})" >+</h2>
-            <h2>${item.quantity}</h2>
-            <h2 onclick="minusitem(${index})" >-</h2>
-        </div>
-        <button onclick="deleteitem(${index})" >delete</button>
-       </div>`
-        
-}
-const plusitem = (item,index)=>{
-        item.quantity +=
-        // productsdiv.innerHTML = `` 
-        // cart[index].quantity = cart[index].quantity - 1
-        productsdiv.innerHTML += `             
-        <div class="card">
-        <h2>${item.name}</h2>
-        <h2>${item.model}</h2>
-        <h3>price : ${item.price}</h3>
-        <div class="quantity-box">
-            <h2 onclick="plusitem(${index})" >+</h2>
-            <h2>${item.quantity}</h2>
-            <h2 onclick="minusitem(${index})" >-</h2>
-        </div>
-        <button onclick="deleteitem(${index})" >delete</button>
-       </div>`
-}
+            <div class="card">
+            <h2>${item.name}</h2>
+            <h2>${item.model}</h2>
+            <h3>price : ${item.price}</h3>
+            <div class="quantity-box">
+                <h2 onclick="plusitem(${item,index})" >+</h2>
+                <h2>${item.quantity}</h2>
+                <h2 onclick="minusitem(${item,index})" >-</h2>
+            </div>
+            <button onclick="deleteitem(${index})" >delete</button>
+           </div>`
+            localStorage.setItem("cart",JSON.stringify(cart))
+        })}
+const plusitem = (index)=>{
+        productsdiv.innerHTML = `` 
+        cart[index].quantity = cart[index].quantity + 1
+        cart.map((item,index)=>{
+            productsdiv.innerHTML += `             
+                <div class="card">
+                <h2>${item.name}</h2>
+                <h2>${item.model}</h2>
+                <h3>price : ${item.price}</h3>
+                <div class="quantity-box">
+                    <h2 onclick="plusitem(${item,index})" >+</h2>
+                    <h2>${item.quantity}</h2>
+                    <h2 onclick="minusitem(${item,index})" >-</h2>
+                </div>
+                <button onclick="deleteitem(${index})" >delete</button>
+               </div>`
+               localStorage.setItem("cart",JSON.stringify(cart))
+
+})}
